867731bef9a4bcbe0f5136031233e2885dce9e6a
mariaeloisa1013 <mariaeloisa1013@gmail.com>
2023-11-01 11:24:13 +0400

    Exercise 2

---
+++ Chapter 4- Control Flow/exercise2.py
@@ -0,0 +1,7 @@
+alien = str(input("What's the color of your alien? "))
+five = "green"
+
+if alien == five:
+    print ("\tyou just earned 5 points!!")
+else :
+    print ("\tyou just earned 10 points!!")

727991ede08f1ecf88f8b71be152ed59b50141e1
mariaeloisa1013 <144213705+mariaeloisa1013@users.noreply.github.com>
2023-11-05 10:34:48 +0400

    Add files via upload

---
+++ Chapter 5- Dictionaries/exercise2.py
@@ -0,0 +1,17 @@
+print("\n\tGlossary")
+glossary = {"integer": "a whole number ",
+    "float": "numbers with decimal",
+    "string": "text data",
+    "variables": "a value",
+    "statement": "a line of code"}
+
+word = "integer"
+print("\n" + word.title() + ":" + glossary[word])
+word = "float"
+print("\n" + word.title() + ":" + glossary[word])
+word = "string"
+print("\n" + word.title() + ":" + glossary[word])
+word = "variables"
+print("\n" + word.title() + ":" + glossary[word])
+word = "statement"
+print("\n" + word.title() + ":" + glossary[word])
---
+++ Chapter 5- Dictionaries/exercise5.py
@@ -0,0 +1,31 @@
+#dictionries: cat, dog, bird, turtle, fish. store dictionaries into pets. loop the list . everything you know\
+pets = []
+animal = {'name':'ramram',
+          'animal':'cat',
+          'breed':'scottish fold',
+          'owner':'maria'}
+pets.append(animal)
+
+animal = {'name':'louie',
+          'animal':'dog',
+          'breed':'german sheperd',
+          'owner':'eloisa'}
+pets.append(animal)
+
+animal = {'name':'jeep',
+          'animal':'turtle',
+          'breed':'snapping turtle',
+          'owner':'pejero'}
+pets.append(animal)
+
+animal = {'name':'goldy',
+          'animal':'fish',
+          'breed':'goldfish',
+          'owner':'butaslac'}
+pets.append(animal)
+
+for animal in pets:
+    print("everything i know about " + animal['name'].title() + ":")
+    for x,y in animal.items():
+        print("\t" + x + ": " + y )
+    print("\n")

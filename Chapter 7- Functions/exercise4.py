da28e462ff94d18e82e3b118b07d230710e80a16
mariaeloisa1013 <mariaeloisa1013@gmail.com>
2023-11-16 20:28:44 +0400

    CH7 exercise 4

---
+++ Chapter 7- Functions/exercise4.py
@@ -0,0 +1,7 @@
+def make_shirt(size= 'large', msg = 'I love Python'):
+    print("Size: " + size)
+    print("Message: " + msg)
+
+make_shirt()
+make_shirt(size='medium')
+make_shirt('small', 'I love SG')

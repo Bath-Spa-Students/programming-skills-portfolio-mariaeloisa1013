c481767f8fb798df7298f46fe9f498db8ad463b2
mariaeloisa1013 <mariaeloisa1013@gmail.com>
2023-11-16 20:28:40 +0400

    CH7 exercise 3

---
+++ Chapter 7- Functions/exercise3.py
@@ -0,0 +1,6 @@
+def make_shirt(size, msg):
+    print ("Your graphic shirt," + msg + " is sized " + size )
+
+
+make_shirt('Extra Small', 'I Love SG')
+make_shirt(size='Extra Large', msg= 'Beautiful')
